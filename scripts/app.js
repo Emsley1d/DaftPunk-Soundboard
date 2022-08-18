5080b0bd38c73802fd03d0fe35688667b4e98af8
emsleyd <emsleyd@gmail.com>
2022-08-18 18:06:31 +0100

    Final Submission

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,64 @@
 
 // ROW 1
-var work= new Audio ();
-work.src="sounds/work_it.wav";
+var work = new Audio();
+work.src = "sounds/work_it.wav";
 
-var make= new Audio ();
-make.src="sounds/make_it.wav";
+var make = new Audio();
+make.src = "sounds/make_it.wav";
 
-var doIt= new Audio ();
-doIt.src="sounds/do_it.wav";
+var doIt = new Audio();
+doIt.src = "sounds/do_it.wav";
 
-var makesUs= new Audio ();
-makesUs.src="sounds/makes_us.wav";
+var makesUs = new Audio();
+makesUs.src = "sounds/makes_us.wav";
 
 
 // ROW 2
-var harder= new Audio ();
-harder.src="sounds/harder.wav";
+var harder = new Audio();
+harder.src = "sounds/harder.wav";
 
-var better= new Audio ();
-better.src="sounds/better.wav";
+var better = new Audio();
+better.src = "sounds/better.wav";
 
-var faster= new Audio ();
-faster.src="sounds/faster.wav";
+var faster = new Audio();
+faster.src = "sounds/faster.wav";
 
-var stronger= new Audio ();
-stronger.src="sounds/stronger.wav";
+var stronger = new Audio();
+stronger.src = "sounds/stronger.wav";
 
 // ROW 3
-var more= new Audio ();
-more.src="sounds/more_than.wav";
+var more = new Audio();
+more.src = "sounds/more_than.wav";
 
-var hour= new Audio ();
-hour.src="sounds/hour.wav";
+var hour = new Audio();
+hour.src = "sounds/hour.wav";
 
-var our= new Audio ();
-our.src="sounds/our.wav";
+var our = new Audio();
+our.src = "sounds/our.wav";
 
-var never = new Audio ();
-never.src="sounds/never.wav";
+var never = new Audio();
+never.src = "sounds/never.wav";
 
 // ROW 4
-var ever= new Audio ();
-ever.src="sounds/ever.wav";
+var ever = new Audio();
+ever.src = "sounds/ever.wav";
 
-var after= new Audio ();
-after.src="sounds/after.wav";
+var after = new Audio();
+after.src = "sounds/after.wav";
 
-var workIs= new Audio ();
-workIs.src="sounds/work_is.wav";
+var workIs = new Audio();
+workIs.src = "sounds/work_is.wav";
 
-var over= new Audio ();
-over.src="sounds/over.wav";
+var over = new Audio();
+over.src = "sounds/over.wav";
+
+
+$('p').mouseover(function () {
+    let box = $(this);
+    box.css({ backgroundColor: 'rgba(225, 225, 225, 0.75)', color: "black", 'border-left': '3px solid white', 'border-bottom': '3px solid white'});
+});
+
+$('p').mouseleave(function () {
+    let box = $(this);
+    box.css({ backgroundColor: 'rgba(225, 225, 225, 0.5)' , color: 'white', 'border-left': '0px', 'border-bottom': '0px'});
+});
